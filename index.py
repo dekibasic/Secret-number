757e57626255eeae65d88255b1167e4b6a514114
dejanbasic <dejan555@gmail.com>
2019-04-13 20:33:03 +0200

    while loop

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 print("Welcome in 'Guess th secret number' game!")
 
 secret_number = 5
-guess = int(input("Enter number between 1 and 20: "))
+guess = 0
 
-if guess == secret_number:
-    print("Congratulations, you guessed secret number!")
-else:
-    print("Sorry, more luck next time!")
-
-print("Thank you for playing our game!")
+while guess != secret_number:
+    guess = int(input("Enter number between 1 and 20: "))
+    if guess == secret_number:
+        print("Congratulations, you guessed secret number!")
+    else:
+        print("Sorry, secret number is not " + str(guess))
